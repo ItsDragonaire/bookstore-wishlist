dd468967d0378fade9ad64c07eb54f33665bbb2b
ItsDragonaire <104574867+ItsDragonaire@users.noreply.github.com>
2026-05-08 20:03:40 +0530

    Delete js directory

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = 'bookstoreWishlistData';
-
-export function loadBooks() {
-  return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-}
-
-export function saveBooks(data) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-}
